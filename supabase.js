6b507a03cc5d4e53e71cc8ec2d3945a761fe0e8f
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2025-11-27 20:07:14 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-// Supabase inicializado via CDN — NÃO usar import/export
+// Configuração segura com Supabase via CDN
 
 const SUPABASE_URL = "https://febwinynlbviadasgwlg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYndpbnlubGJ2aWFkYXNnd2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjI3MjMsImV4cCI6MjA3OTczODcyM30.WubNRAHhlH6wnjGAWc4B7oHEbrU9fd1xU5uWWqRjE4c";
