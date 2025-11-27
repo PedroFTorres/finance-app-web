f4e0c8e3ab9327f7f80a5bdc4abf84f3cf63b8ac
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2025-11-27 15:28:28 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,4 @@
-// supabase.js
-
 const SUPABASE_URL = "https://febwinynlbviadasgwlg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYndpbnlubGJ2aWFkYXNnd2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjI3MjMsImV4cCI6MjA3OTczODcyM30.WubNRAHhlH6wnjGAWc4B7oHEbrU9fd1xU5uWWqRjE4c";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
