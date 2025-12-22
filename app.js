2fce7435736b1bc9b9b10cb788ce8622c0e0ba55
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2025-12-22 15:12:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -876,6 +876,7 @@ await LancService.insert({
         console.error('handleSaveModal', e);
         alert('Erro ao salvar lançamento. Veja console.');
       }
+    }
 
   /* ============================
      CHARTS
