867b1f3ad503ae4409acd5dae10d1de0077f44f5
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2026-02-17 19:50:13 -0300

    Update login.js

--- login.js
+++ login.js
@@ -2,20 +2,8 @@ btnSignup = document.getElementById("btn-signup");
 btnSignin = document.getElementById("btn-signin");
 msg = document.getElementById("msg");
 
-btnSignup.onclick = async () => {
-  let email = document.getElementById("email").value;
-  let pass = document.getElementById("password").value;
-
-  const { error } = await supabase.auth.signUp({ email, password: pass });
-
-  if (error) {
-  msg.textContent = error.message;
-  return;
-}
-
-msg.textContent =
-  "Conta criada! Verifique seu email para confirmar antes de entrar.";
-
+btnSignup.onclick = () => {
+  document.getElementById("modal-signup").classList.remove("hidden");
 };
 
 btnSignin.onclick = async () => {
