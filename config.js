8902160f1edb6db3f53ae229975ae89ecf643501
PedroFTorres <137004613+PedroFTorres@users.noreply.github.com>
2026-06-11 19:55:11 -0300

    Set PRO price to final amount

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://febwinynlbviadasgwlg.supabase.co",
   MERCADO_PAGO_PUBLIC_KEY: "APP_USR-db93fa0b-e99b-4861-abea-d82a0d9ffb6c",
-  PRO_PRICE_BRL: 5,
+  PRO_PRICE_BRL: 19.9,
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYndpbnlubGJ2aWFkYXNnd2xnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNjI3MjMsImV4cCI6MjA3OTczODcyM30.WubNRAHhlH6wnjGAWc4B7oHEbrU9fd1xU5uWWqRjE4c"
 };
